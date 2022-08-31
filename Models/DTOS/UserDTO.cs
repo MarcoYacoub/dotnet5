d2b25644f1d7a89bc9df511f6e460af7d8a12707
Marco Yacoub <myacoub@turndigital.net>
2022-08-31 13:45:29 +0200

    initial commit

---
+++ Models/DTOS/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.DTOS
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int age { get; set; }
+
+    }
+}
--- dotnet5/Pages/Index.cshtml.cs
+++ dotnet5/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Models.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace dotnet5.Pages
 {
     public class IndexModel : PageModel
     {
+        public User user { get; set; }
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -19,7 +21,13 @@ namespace dotnet5.Pages
 
         public void OnGet()
         {
+            user = new User
+            {
+                age = 15,
+                Id = 15,
+                Name = "15 string"
 
+            };
         }
     }
 }
